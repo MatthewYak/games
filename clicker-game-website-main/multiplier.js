2e8a1023292b27840bc67215341a0f68e50a3120
Matthew Yak <matthew@matthewyak.com>
2022-11-04 20:16:28 -0700

    New logic and styles

---
+++ clicker-game-website-main/multiplier.js
@@ -0,0 +1,14 @@
+document.getElementById('Multiplier').innerText = 'Upgrade Multiplier ($' + (multiplierCost) + ')'
+
+function Upgrade() {
+  if (balance >= (multiplierCost * multiplierCostMultiplier)) {
+
+    multiplier += 0.5 * multiplierCostMultiplier;
+    balance -= (multiplierCost * multiplierCostMultiplier);
+
+    multiplierCostMultiplier += 1;
+    multiplierCost = (multiplierCost * multiplierCostMultiplier);
+    updateCount()
+    document.getElementById('Multiplier').innerText = 'Upgrade Multiplier ($' + (multiplierCost * multiplierCostMultiplier) + ')'
+  }
+}
--- clicker-game-website-main/scripts.js
+++ clicker-game-website-main/scripts.js
@@ -1,28 +1,8 @@
 let balance = 0;
-let multiplier = 0.5;
+let multiplier = 1;
 
-function Click() {
-  balance += (1 * multiplier);
-  updateCount();
-  document.getElementById('collectmoney').hidden = true;
-  document.getElementById('collectmoney').hidden = false;
-}
-
-
-var multiplierCostMultiplier = 1;
-
-function Upgrade() {
-  if (balance >= (10 * multiplierCostMultiplier)) {
-
-    multiplier += 0.5 * multiplierCostMultiplier;
-    balance -= (10 * multiplierCostMultiplier);
-
-    multiplierCostMultiplier += 1;
-    multiplierCost = (10 * multiplierCostMultiplier);
-    updateCount()
-    document.getElementById('Multiplier').innerText = 'Upgrade Multiplier ($' + (multiplierCost) + ')'
-  }
-}
+let multiplierCost = 10;
+let multiplierCostMultiplier = 1;
 
 function updateCount() {
   document.getElementById('Text').innerText = '$' + balance;
