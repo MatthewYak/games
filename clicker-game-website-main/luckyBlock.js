3e084578bd5884b5d3798a3fe81896b1cba38585
Matthew Yak <matthew@matthewyak.com>
2022-11-05 21:23:46 -0700

    Update luckyBlock.js

--- clicker-game-website-main/luckyBlock.js
+++ clicker-game-website-main/luckyBlock.js
@@ -1,16 +1,19 @@
 function BuyLuckyBlock() {
-	if (balance >= 100 * multiplier && multiplier > 0) {
-		balance -= 100 * multiplier;
-		updateCount();
-		let random = Math.floor(Math.random() * options.length);
-		options[random].effect();
-		document.getElementById("luckyBlockMessage").innerText =
-			options[random].message;
-		document.getElementsByClassName("moneyPanelText")[0].hidden = true;
+  if (balance >= 100 * multiplier && multiplier > 0) {
+    balance -= 100 * multiplier;
+    updateCount();
+    let random = Math.floor(Math.random() * options.length);
+    options[random].effect();
+    document.getElementById("luckyBlockMessage").innerText =
+      options[random].message;
+    document.getElementsByClassName("moneyPanelText")[0].hidden = true;
 
-		setTimeout(() => {
-			document.getElementsByClassName("moneyPanelText")[0].hidden = false;
-			document.getElementById("luckyBlockMessage").innerText = "";
-		}, 5000);
-	}
+    document.getElementById('luckyBlock').disabled = true;
+
+    setTimeout(() => {
+      document.getElementsByClassName("moneyPanelText")[0].hidden = false;
+      document.getElementById("luckyBlockMessage").innerText = "";
+      document.getElementById('luckyBlock').disabled = false;
+    }, 5000);
+  }
 }
