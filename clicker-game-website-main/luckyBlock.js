3464257e578c3e9cd5b73e5d553a8baa302268df
Matthew Yak <matthew@matthewyak.com>
2022-11-06 12:40:07 -0800

    Updated options

--- clicker-game-website-main/luckyBlock.js
+++ clicker-game-website-main/luckyBlock.js
@@ -1,9 +1,10 @@
 function BuyLuckyBlock() {
   if (balance >= 100 * multiplier && multiplier > 0) {
+    let luckyBlockCost = 100 * multiplier;
     balance -= 100 * multiplier;
     updateCount();
     let random = Math.floor(Math.random() * options.length);
-    options[random].effect();
+    options[random].effect(luckyBlockCost);
     document.getElementById("luckyBlockMessage").innerText =
       options[random].message;
     document.getElementsByClassName("moneyPanelText")[0].hidden = true;
